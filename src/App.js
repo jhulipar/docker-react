76009f87e37840dab95a8272969c58e3c3dcb30f
Jhul Ipar <101224039+jhulipar@users.noreply.github.com>
2025-01-16 17:11:38 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>bye world!</p>
+        <p>Hello world!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
